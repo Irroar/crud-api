fa7a3b4bf24df74419788e63b9710e7227909765
Iryna Lytvynenko <frau4145@gmail.com>
2023-04-04 15:19:48 -0400

    add: include up to date files

---
+++ src/database.ts
@@ -0,0 +1,26 @@
+import { User } from './models/userModel';
+
+export class DataBase {
+    users: User[] = [];
+    
+    async getAll() {
+        return this.users;
+    }
+
+    async getById(id: string) {
+        return this.users.find((p) => p.id === id);
+    }
+
+    async add(product: User) {
+        return this.users.push(product);
+    }
+
+    async update(id: string, product) {
+        const index = this.users.findIndex((p) => p.id === id);
+        return this.users[index] = {id, ...product};
+    }
+
+    async delete(id: string) {
+        return this.users = this.users.filter((p) => p.id != id); 
+    }
+} 
